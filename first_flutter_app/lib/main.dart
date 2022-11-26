d42d032ff02d2ed3a9a8e0242d0f381b1b59d580
FasialG <117646880+FasialG@users.noreply.github.com>
2022-11-27 01:11:37 +0300

    Done

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -1,115 +1,238 @@
+// ignore_for_file: camel_case_types
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+main() {
+  runApp(const FirstFlutterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FirstFlutterApp extends StatelessWidget {
+  const FirstFlutterApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return const MaterialApp(home: home());
+  }
+}
+
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Assainment"),
+        centerTitle: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: const Body(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Body extends StatelessWidget {
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+    return ListView(
+      children: const [
+        SizedBox(
+          height: 25,
+        ),
+        columF(),
+        SizedBox(
+          height: 25,
+        ),
+        cloumS(),
+        SizedBox(
+          height: 25,
+        ),
+        cloumT(),
+        SizedBox(
+          height: 75,
+        ),
+        text(),
+      ],
+    );
+  }
+}
+
+// ignore: camel_case_types
+class columF extends StatelessWidget {
+  const columF({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          children: [
+            Container(
+              height: 100,
+              width: 120,
+              alignment: Alignment.center,
+              color: Colors.red,
+              child: const Text(
+                "First",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
+  }
+}
+
+class cloumS extends StatelessWidget {
+  const cloumS({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            height: 100,
+            width: 120,
+            alignment: Alignment.topLeft,
+            color: Colors.green,
+            padding: const EdgeInsets.all(8),
+            child: const Text(
+              "Second",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 15),
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 120,
+            alignment: Alignment.bottomLeft,
+            color: Colors.brown,
+            padding: const EdgeInsets.all(8),
+            child: const Text(
+              "Third",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15),
+            ),
+          )
+        ],
+      ),
+    ]);
+  }
+}
+
+class cloumT extends StatelessWidget {
+  const cloumT({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 100,
+              width: 120,
+              alignment: Alignment.topRight,
+              color: Colors.grey,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                "Fourth",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 120,
+              alignment: Alignment.bottomRight,
+              color: Colors.blue[900],
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                "Fifth",
+                style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 120,
+              alignment: Alignment.topCenter,
+              color: Colors.red,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                "Sixth",
+                style: TextStyle(
+                    color: Colors.orange,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class text extends StatelessWidget {
+  const text({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(
+        children: [
+          Container(
+            width: 120,
+            height: 120,
+            padding: const EdgeInsets.only(left: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('Name :',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
+                Text('Email :',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                Text('Age :',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15))
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 120,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('Faisal Alharbi', style: TextStyle(fontSize: 25)),
+                Text('Alharbi.Faisal.G@gmail.com',
+                    style: TextStyle(fontSize: 20)),
+                Text('25', style: TextStyle(fontSize: 15))
+              ],
+            ),
+          )
+        ],
+      )
+    ]);
   }
 }
